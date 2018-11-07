c4ef34f1d301ae4c8db0eefcb774ffeee3116dc1
Kale Kundert <kale@thekunderts.net>
2018-11-07 14:02:15 -0800

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,14 @@
+Bio96
+===============================
+
+.. image:: https://img.shields.io/pypi/v/bio96.svg
+   :target: https://pypi.python.org/pypi/bio96
+
+.. image:: https://img.shields.io/pypi/pyversions/bio96.svg
+   :target: https://pypi.python.org/pypi/bio96
+
+.. image:: https://img.shields.io/travis/kalekundert/bio96.svg
+   :target: https://travis-ci.org/kalekundert/bio96
+
+.. image:: https://img.shields.io/coveralls/kalekundert/bio96.svg
+   :target: https://coveralls.io/github/kalekundert/bio96?branch=master
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import re
+with open('bio96/__init__.py') as file:
+    version_pattern = re.compile("__version__ = '(.*)'")
+    version = version_pattern.search(file.read()).group(1)
+
+with open('README.rst') as file:
+    readme = file.read()
+
+setup(
+    name='bio96',
+    version=version,
+    author='Kale Kundert',
+    author_email='kale@thekunderts.net',
+    description='',
+    long_description=readme,
+    url='https://github.com/kalekundert/bio96',
+    packages=[
+        'bio96',
+    ],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords=[
+        'bio96',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
