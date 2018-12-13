0a49d47647ee28f4fd79b7a22edfc4168d1030d9
Kale Kundert <kale@thekunderts.net>
2018-12-13 12:12:35 -0800

    Remove old cookiecutter references.

--- docs/conf.py
+++ docs/conf.py
@@ -190,7 +190,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'cookiecutterproject_namedoc'
+htmlhelp_basename = 'bio96'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -210,8 +210,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'cookiecutterproject_name.tex', u'\\{\\{ cookiecutter.project\\_name \\}\\} Documentation',
-   u'\\{\\{ cookiecutter.full\\_name \\}\\}', 'manual'),
+  ('index', 'bio96.tex', 'bio96 Documentation',
+   'bio96', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -240,7 +240,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'cookiecutterproject_name', u'bio96 Documentation',
+    ('index', 'bio96', u'bio96 Documentation',
      [u'Kale Kundert'], 1)
 ]
 
@@ -254,8 +254,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'cookiecutterproject_name', u'bio96 Documentation',
-   u'Kale Kundert', 'cookiecutterproject_name', 'One line description of project.',
+  ('index', 'bio96', u'bio96 Documentation',
+   u'Kale Kundert', 'bio96', 'File format for 96-well plate layouts',
    'Miscellaneous'),
 ]
 
