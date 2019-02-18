919d9dbe3abc313d50f4ca58b9dab7c48264da11
Kale Kundert <kale@thekunderts.net>
2019-02-18 01:30:21 -0800

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as file:
 
 setup(
     name='bio96',
-    version='0.4.0',
+    version='0.5.0',
     author='Kale Kundert',
     author_email='kale@thekunderts.net',
     description="File format for 96-well plate layouts",
