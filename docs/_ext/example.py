1834690302b7dbe2399d1420fec89c1623237e02
Kale Kundert <kale@thekunderts.net>
2020-06-09 17:16:11 -0400

    Don't overwrite example SVGs that haven't changed.

--- docs/_ext/example.py
+++ docs/_ext/example.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import os
+import wellmap
 import shlex
 
 from docutils import nodes
 from docutils.statemachine import StringList
 from sphinx.util.docutils import SphinxDirective
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
 
 class Example(SphinxDirective):
     required_arguments = 1
@@ -23,23 +27,19 @@ class Example(SphinxDirective):
         example_rst = ""
 
         if not self.content:
-            content = [None] * len(rel_paths)
+            contents = [None] * len(rel_paths)
         else:
-            content = '\n'.join(self.content).split('--EOF--')
+            contents = '\n'.join(self.content).split('--EOF--')
 
-        if len(content) != len(rel_paths):
+        if len(contents) != len(rel_paths):
             raise self.error(f"found {len(content)} TOML snippets, but {len(rel_paths)} paths.")
 
-        for rel_path, content in zip(rel_paths, content):
+        for rel_path, content in zip(rel_paths, contents):
             toml_path, toml_abs_path = self.env.relfn2path(rel_path)
-            svg_path = change_ext(toml_path, '.svg')
-            svg_abs_path = change_ext(toml_abs_path, '.svg')
             name = os.path.basename(toml_path)
 
             if content:
-                os.makedirs(os.path.dirname(toml_abs_path), exist_ok=True)
-                with open(toml_abs_path, 'w') as f:
-                    f.write(content)
+                update_toml_file(toml_abs_path, content)
 
             if not os.path.exists(toml_abs_path):
                 raise self.error(f"no such file: {toml_path}")
@@ -50,16 +50,19 @@ class Example(SphinxDirective):
     :caption: :download:`{name} </{toml_path}>`
 '''
 
+        # Only make a figure for the last snippet.  
         if 'no-figure' not in self.options:
-            import wellmap
-            import colorcet
-            import matplotlib.pyplot as plt
+            svg_path = change_ext(toml_path, '.svg')
+            svg_abs_path = change_ext(toml_abs_path, '.svg')
 
-            df = wellmap.load(toml_abs_path)
-            attrs = self.options.get('attrs', [])
-            cmap = wellmap.verify.get_colormap(self.options.get('colors', 'rainbow'))
-            fig = wellmap.verify.plot_layout(df, attrs, cmap=cmap)
-            fig.savefig(svg_abs_path, bbox_inches='tight')
+            df, deps = wellmap.load(toml_abs_path, report_dependencies=True)
+
+            if any_deps_stale(svg_abs_path, deps):
+                logger.info(f"[example] rendering: {svg_path}")
+                attrs = self.options.get('attrs', [])
+                cmap = wellmap.plot.get_colormap(self.options.get('color', 'rainbow'))
+                fig = wellmap.plot.plot_layout(df, attrs, cmap)
+                fig.savefig(svg_abs_path, bbox_inches='tight')
 
             example_rst += f'''\
 .. figure:: /{svg_path}
@@ -72,6 +75,34 @@ class Example(SphinxDirective):
 
 def change_ext(path, new_ext):
     return os.path.splitext(path)[0] + new_ext
+
+def update_toml_file(toml_abs_path, content):
+    # Don't write the file if its contents wouldn't change.  This is important, 
+    # because otherwise we won't know whether or not the SVG file is stale.
+    try:
+        with open(toml_abs_path) as f:
+            prev_content = f.read()
+        if content == prev_content:
+            return
+    except FileNotFoundError:
+        pass
+
+    os.makedirs(os.path.dirname(toml_abs_path), exist_ok=True)
+    with open(toml_abs_path, 'w') as f:
+        f.write(content)
+
+def any_deps_stale(svg_abs_path, toml_abs_paths):
+    try:
+        for toml_abs_path in toml_abs_paths:
+            toml_mtime = os.path.getmtime(toml_abs_path)
+            svg_mtime = os.path.getmtime(svg_abs_path)
+            if toml_mtime > svg_mtime:
+                return True
+
+    except FileNotFoundError:
+        return True
+
+    return False
 
 def setup(app):
     app.add_directive('example', Example)
